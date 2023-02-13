2ca9d3a58b7c32da0b1f0142634406b064ad7f8c
Victor Olamide <akinnusivictor098@gmail.com>
2023-02-13 15:29:47 +0100

    "New Update"

--- src/main/java/com/EmployeeManagementSystem/volacode/repositories/EmployeeRepository.java
+++ src/main/java/com/EmployeeManagementSystem/volacode/repositories/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.EmployeeManagementSystem.volacode.repositories;
 
 import com.EmployeeManagementSystem.volacode.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }
